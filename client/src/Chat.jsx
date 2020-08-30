70130732568ef438678728d1360829adaed1390b
Annie Huang <annie.hn@gmail.com>
2020-08-30 17:50:30 +1000

    Import <ApolloProvider> to wrapped around the Chat component.

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+// Mirror how to do it in https://www.apollographql.com/docs/react/get-started/ from index.js setup;
+// 'Connect your client to React' setup.
+import { ApolloClient, InMemoryCache, ApolloProvider  } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache()
 });
+
+const Chat = () => {
+  return (
+    <div>I'm a chat window</div>
+  )
+}
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Chat />
+  </ApolloProvider>
+)
