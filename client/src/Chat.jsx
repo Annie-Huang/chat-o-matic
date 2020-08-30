b132cb16771708a1940e47038918c853cb1b4e39
Annie Huang <annie.hn@gmail.com>
2020-08-30 17:45:10 +1000

    Copy local GraphQL server url into Chat.jsx

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'http://localhost:4000/',
   cache: new InMemoryCache()
 });
