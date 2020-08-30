56a8e6abab58e72baeb2c33ae96729ba24108a14
Annie Huang <annie.hn@gmail.com>
2020-08-30 16:47:53 +1000

    Add mutation into typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const {GraphQLServer} = require('graphql-yoga');
 
+const messages = [];
+
 // typeDefs is like a schema. ID! means the ID field is required.
+// mutation is list post in the rest world
 const typeDefs = `
   type Message {
     id: ID!
@@ -11,12 +14,27 @@ const typeDefs = `
   type Query {
     messages: [Message!]
   }
+  
+  type Mutation {
+    postMessage(user: String!, content: String!): ID!
+  }
 `;
 
 // How do I get the data (through resolvers). Need to match the keys in the type definition
 const resolvers = {
   Query: {
     messages: () => messages,
+  },
+  Mutation: {
+    postMessage: (parent, {user, content}) => {
+      const id = messages.length;
+      messages.push({
+        id,
+        user,
+        content
+      });
+      return id;
+    }
   }
 }
 const server = new GraphQLServer({ typeDefs, resolvers });
