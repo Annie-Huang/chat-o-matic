b425fe1733ddd208ce9792dd0a06c3bf1e97928f
Annie Huang <annie.hn@gmail.com>
2020-08-30 18:18:33 +1000

    Create a graphQL query constant GET_MESSAGES

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
-// Mirror how to do it in https://www.apollographql.com/docs/react/get-started/ from index.js setup;
-// 'Connect your client to React' setup.
-import { ApolloClient, InMemoryCache, ApolloProvider  } from '@apollo/client';
+// Mirror how to do it in https://www.apollographql.com/docs/react/get-started/ from
+// 'index.js' setup | 'Connect your client to React' setup | 'Request data' example
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql  } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache()
 });
 
+const GET_MESSAGES = gql`
+query {
+  messages {
+    id
+    content
+    user
+  }
+}
+`;
+
+// <div>I'm a chat window</div>
 const Chat = () => {
   return (
     <div>I'm a chat window</div>
