d5f690e5dced6c54bb1e35a648259f874e30624a
Annie Huang <annie.hn@gmail.com>
2020-08-30 18:50:31 +1000

    Style the Messages component to display the msg on the right with green color if the user is the user stored in the DB. And display grey color on the left if the user is not the user stored in the DB.

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 // 'index.js' setup | 'Connect your client to React' setup | 'Request data' example
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql  } from '@apollo/client';
 
+import {Container} from 'shards-react';
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache()
@@ -25,13 +27,40 @@ const Messages = ({user}) => {
     return null;
   }
 
-  return JSON.stringify(data);
+  // return JSON.stringify(data);
+  return (
+    <>
+      {data.messages.map(({id, user: messageUser, content}) => (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em',
+          }}
+        >
+          <div
+            style={{
+              background: user === messageUser ? '#58bf56' : '#e5e6ea',
+              color: user === messageUser ? 'white' : 'black',
+              padding: '1em',
+              borderRadius: '1em',
+              maxWidth: '60%'
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
 // <div>I'm a chat window</div>
 const Chat = () => {
   return (
-    <div><Messages user='Jack' /></div>
+    <Container>
+      <Messages user='Jack' />
+    </Container>
   )
 }
 
